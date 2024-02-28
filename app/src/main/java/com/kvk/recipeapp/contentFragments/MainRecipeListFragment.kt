f82f053bdd62d8f577e0f8e1607d1e045f98063d
Dainius258 <dainiuss258@gmail.com>
2024-02-28 20:32:15 +0200

    Tweaked item_recipe.xml

--- app/src/main/java/com/kvk/recipeapp/contentFragments/MainRecipeListFragment.kt
+++ app/src/main/java/com/kvk/recipeapp/contentFragments/MainRecipeListFragment.kt
@@ -19,19 +19,32 @@ class MainRecipeListFragment : Fragment() {
     ): View? {
 
         var recipeList = mutableListOf(
-            Recipe("Delicious boorgir", false),
-            Recipe("Delicious boorgir", false),
-            Recipe("Delicious boorgir", false),
-            Recipe("Delicious boorgir", false),
-            Recipe("Delicious boorgir", false),
-            Recipe("Delicious boorgir", false),
-            Recipe("Delicious boorgir", false),
+            Recipe("Spaghetti Carbonara", false),
+            Recipe("Chicken Alfredo", false),
+            Recipe("Beef Stroganoff", false),
+            Recipe("Shrimp Scampi", false),
+            Recipe("Chicken Parmesan", false),
+            Recipe("Beef Wellington", false),
+            Recipe("Mushroom Risotto", false),
+            Recipe("Pad Thai", false),
+            Recipe("Sushi Rolls", false),
+            Recipe("Lamb Tagine", false),
+            Recipe("Eggplant Parmesan", false),
+            Recipe("Chicken Curry", false),
+            Recipe("Tacos", false),
+            Recipe("Salmon Teriyaki", false),
+            Recipe("Beef Tacos", false),
+            Recipe("Vegetable Stir-Fry", false),
+            Recipe("Chicken Fajitas", false),
+            Recipe("Pasta Primavera", false),
+            Recipe("Vegetable Lasagna", false),
+            Recipe("Spinach Salad", false)
         )
 
         val rootView = inflater.inflate(R.layout.fragment_main_recipe_list, container, false)
         val recyclerView: RecyclerView = rootView.findViewById(R.id.rvRecipes)
 
-        val layoutManager = GridLayoutManager(context, 2)
+        val layoutManager = GridLayoutManager(context, 1)
         recyclerView.layoutManager = layoutManager
 
         val adapter = RecipeAdapter(recipeList)
